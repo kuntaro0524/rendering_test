bce7dceefd8bd1226291c7875b7ec7ba6636e530
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-25 16:14:27 +0900

    ボタンをクリックしたら子が出る という仕掛けを実装しました。

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,26 @@ import { ChildArea } from "./ChildArea";
 export default function App() {
   console.log("App!");
 
+  // メイン画面のインプットのテキストを取得するためのuseState
   const [text, setText] = useState("");
+  // 子のコンポーネントを表示するかしないかを選択するための　useState
+  const [open, setOpen] = useState(false);
 
   const onChangeText = (e) => {
     setText(e.target.value);
   };
+
+  const onClickCheck = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
-      <button> PUSHME </button>
-      <ChildArea />
+      <button onClick={onClickCheck}> PUSHME </button>
+      {open ? <ChildArea /> : null}
     </div>
   );
 }
