a46de8975703b8ad844671ec0a5cf78649955641
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-25 15:54:56 +0900

    最初のコード 単純にuseStateでカウンターをセットした

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const pushClick = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
+      <p> {count} </p>
+      <button onClick={pushClick}> count up </button>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
